d77bd3a8be1da56edcbcb017d607f4bb64f04f5d
kendlbat <kendlbat@edu.htl-villach.at>
2023-12-16 22:49:27 +0100

    More better day 16

--- 16/01.py
+++ 16/01.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from common import getEnergisedCount, Direction
 
 print(getEnergisedCount(0, 0, Direction.RIGHT))
--- 16/02.py
+++ 16/02.py
@@ -1,19 +1,12 @@
+#!/usr/bin/env python3
 from common import getEnergisedCount, getInput, Direction
 
 width = len(getInput()[0])
 height = len(getInput())
 
-# Left starters
-ec = max([getEnergisedCount(0, y, Direction.RIGHT) for y in range(height)])
-
-# Top starters
-ec = max([ec, max([getEnergisedCount(x, 0, Direction.DOWN) for x in range(width)])])
-
-# Right starters
-ec = max([ec, max([getEnergisedCount(width - 1, y, Direction.LEFT) for y in range(height)])])
-
-# Bottom starters
-ec: int = max([ec, max([getEnergisedCount(x, height - 1, Direction.UP) for x in range(width)])])
-
-
-print(ec)
+print(max([
+    max([getEnergisedCount(0, y, Direction.RIGHT) for y in range(height)]),
+    max([getEnergisedCount(x, 0, Direction.DOWN) for x in range(width)]),
+    max([getEnergisedCount(width - 1, y, Direction.LEFT) for y in range(height)]),
+    max([getEnergisedCount(x, height - 1, Direction.UP) for x in range(width)])
+]))
