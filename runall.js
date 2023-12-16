d362232e8fb2a7c5e2bac88243753d3d77ad7f74
kendlbat <kendlbat@edu.htl-villach.at>
2023-12-16 22:36:10 +0100

    Async-er runall

--- runall.js
+++ runall.js
@@ -11,38 +11,44 @@ async function main() {
         .filter((name) => name.match(/^(0[1-9]|1[0-9]|2[0-5])$/));
 
     const times = await Promise.allSettled(
-        dirs.map(async (dir) => {
-            try {
-                if (!fs.existsSync(`${dir}/input`)) {
-                    const input = await aoc.getInput(dir);
-                    fs.writeFileSync(`${dir}/input`, input);
-                    console.log(`Fetched input for ${dir}`);
-                }
+        dirs.map(
+            (dir) =>
+                new Promise(async (resolve, reject) => {
+                    setTimeout(async () => {
+                        try {
+                            console.log(`Running day ${dir}`);
+                            if (!fs.existsSync(`${dir}/input`)) {
+                                const input = await aoc.getInput(dir);
+                                fs.writeFileSync(`${dir}/input`, input);
+                                console.log(`Fetched input for ${dir}`);
+                            }
 
-                // Get files in directory
-                const files = fs
-                    .readdirSync(dir)
-                    .filter((x) => x.match(/\.(js|py)$/));
+                            // Get files in directory
+                            const files = fs
+                                .readdirSync(dir)
+                                .filter((x) => x.match(/\.(js|py)$/));
 
-                const start1 = Date.now();
-                if (files.includes("01.js")) {
-                    cp.execSync(`cd ${dir} && node 01.js`);
-                } else if (files.includes("01.py")) {
-                    cp.execSync(`cd ${dir} && python3 01.py`);
-                }
-                const end1 = Date.now();
-                if (files.includes("02.js")) {
-                    cp.execSync(`cd ${dir} && node 02.js`);
-                } else if (files.includes("02.py")) {
-                    cp.execSync(`cd ${dir} && python3 02.py`);
-                }
-                const end2 = Date.now();
+                            const start1 = Date.now();
+                            if (files.includes("01.js")) {
+                                cp.execSync(`cd ${dir} && node 01.js`);
+                            } else if (files.includes("01.py")) {
+                                cp.execSync(`cd ${dir} && python3 01.py`);
+                            }
+                            const end1 = Date.now();
+                            if (files.includes("02.js")) {
+                                cp.execSync(`cd ${dir} && node 02.js`);
+                            } else if (files.includes("02.py")) {
+                                cp.execSync(`cd ${dir} && python3 02.py`);
+                            }
+                            const end2 = Date.now();
 
-                return [end1 - start1, end2 - end1];
-            } catch (e) {
-                // console.warn(`Failed to run day ${dir}`);
-            }
-        }),
+                            resolve([end1 - start1, end2 - end1]);
+                        } catch (e) {
+                            // console.warn(`Failed to run day ${dir}`);
+                        }
+                    }, 0);
+                }),
+        ),
     );
 
     times.forEach((p, idx) => {
